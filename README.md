759980a9cd762b937c95b7fa51a025787f0d32b7
chichunchen <chichunchen844@gmail.com>
2018-04-20 23:14:59 -0400

    Add readme and improve project structure

---
+++ README.md
@@ -0,0 +1,14 @@
+# vros-simulate
+Simulator build for research of virtual reality optimization.
+
+
+## Usage
+First of all, you need a recent version of rust compiler (maybe >= 1.25).
+
+```
+cargo run user_viewport_result/Paris-sJxiPiAaB4k tracedump/paris.txt object_as_cluster/paris.json 0.96 20 1600 1600 2000 2000
+```
+
+## Result
+The data is visualized in the [vros-dataset](https://github.com/horizon-research/vros-dataset).
+And you might be interested in the [summary](https://github.com/horizon-research/vros-dataset/blob/master/heatmap.ipynb) of the data.
--- auto.sh
+++ auto.sh
@@ -6,8 +6,8 @@ do
         # cargo run user_viewport_result/Elephant-training-2bpICIClAIg tracedump/elephant.txt object_as_cluster/elephant.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> elephant_dump.txt &
         # cargo run user_viewport_result/Rhino-training-7IWp875pCxQ tracedump/rhinos.txt object_as_cluster/rhinos.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> rhino_dump.txt &
         # cargo run user_viewport_result/Rollercoaster-8lsB-P8nGSM tracedump/roller.txt object_as_cluster/roller.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> roller_dump.txt &
-        cargo run user_viewport_result/Timelapse-CIw8R8thnm8 tracedump/nyc.txt object_as_cluster/nyc.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> nyc_dump.txt &
-        # cargo run user_viewport_result/Paris-sJxiPiAaB4k tracedump/paris.txt object_as_cluster/paris.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> paris_dump.txt &
+        # cargo run user_viewport_result/Timelapse-CIw8R8thnm8 tracedump/nyc.txt object_as_cluster/nyc.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> nyc_dump.txt &
+        cargo run user_viewport_result/Paris-sJxiPiAaB4k tracedump/paris.txt object_as_cluster/paris.json 0.96 20 ${SCREEN} ${SCREEN} ${LEVEL2} 2160 >> paris_dump.txt &
     done
     wait
 done
